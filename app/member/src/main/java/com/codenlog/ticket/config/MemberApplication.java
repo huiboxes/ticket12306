1071f2668d84ca89af9c6704a5da38c9ce95a749
kylo <huiboxes@gmail.com>
2025-10-02 00:42:30 +0800

    chore: 日志配置

--- app/member/src/main/java/com/codenlog/ticket/config/MemberApplication.java
+++ app/member/src/main/java/com/codenlog/ticket/config/MemberApplication.java
@@ -1,16 +1,27 @@
 package com.codenlog.ticket.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+
 
 @SpringBootApplication
 @ComponentScan("com.codenlog")
 public class MemberApplication {
 
-    public static void main(String[] args) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MemberApplication.class);
 
-        SpringApplication.run(MemberApplication.class, args);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(MemberApplication.class, args);
+        ConfigurableEnvironment env = ctx.getEnvironment();
+
+        LOGGER.info("启动成功~~");
+        LOGGER.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
     }
 
 }
