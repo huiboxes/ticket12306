b5a6f7b9d4ac11c89c1893f6df2dd49449b5e8b8
kylo <huiboxes@gmail.com>
2025-10-02 16:39:28 +0800

    feat: 增加common模块 && 增加请求LogAspect

--- app/member/src/main/java/com/codenlog/ticket/MemberApplication.java
+++ app/member/src/main/java/com/codenlog/ticket/MemberApplication.java
@@ -1,4 +1,4 @@
-package com.codenlog.ticket.config;
+package com.codenlog.ticket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +20,7 @@ public class MemberApplication {
         ConfigurableEnvironment env = ctx.getEnvironment();
 
         LOGGER.info("启动成功~~");
-        LOGGER.info("地址: \thttp://127.0.0.1:{}/{}", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+        LOGGER.info("地址: \thttp://127.0.0.1:{}{}", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
 
     }
 
